c5250b3b2548ca2157809d4779a93b4921fe391a
David Siewert <david1gruppenplan@gmail.com>
2025-09-21 13:54:56 +0600

    refactor: rename getDemoRoutes -> generateDemoRoutes

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,19 @@
 import { demoList } from "@/demos/demoList"
 import { Router } from "@solidjs/router"
 import { render } from "solid-js/web"
-import { getDemoRoutes } from "~/demo_pages/getDemoRoutes"
+import { generateDemoRoutes } from "~/demo_pages/generateDemoRoutes"
 import "./global.css"
 
 const root = document.getElementById("root")
 
-const demoRoutes = getDemoRoutes(demoList, "/")
+const demoRoutes = generateDemoRoutes(demoList, "")
 console.log(demoRoutes)
 
 const demoRoutesDef = [
   {
     // path: "/*",
     // component: LayoutWrapperDemo,
-    children: getDemoRoutes(demoList, ""),
+    children: demoRoutes,
   },
 ]
 
