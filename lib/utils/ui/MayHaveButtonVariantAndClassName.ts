a52618bbcc51606d2bd82972819462e7fd861ecc
David Siewert <david1gruppenplan@gmail.com>
2025-09-28 05:12:00 +0600

    fix build errors

--- lib/utils/ui/MayHaveButtonVariantAndClassName.ts
+++ lib/utils/ui/MayHaveButtonVariantAndClassName.ts
@@ -1,4 +1,4 @@
-import type { MayHaveButtonVariant } from "~/utils/MayHaveButtonVariant.ts"
+import type { MayHaveButtonVariant } from "~/utils/ui/MayHaveButtonVariant.ts"
 import type { MayHaveClass } from "~/utils/ui/MayHaveClass"
 
 export interface MayHaveButtonVariantAndClassName extends MayHaveButtonVariant, MayHaveClass {}
