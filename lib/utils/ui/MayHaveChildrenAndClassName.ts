4f46712449f1b2d6931ae2c9d565fc3134b0e666
David Siewert <david1gruppenplan@gmail.com>
2025-09-28 05:12:00 +0600

    fix: build errors

--- lib/utils/ui/MayHaveChildrenAndClassName.ts
+++ lib/utils/ui/MayHaveChildrenAndClassName.ts
@@ -1,4 +1,4 @@
-import type { MayHaveChildren } from "~/utils/MayHaveChildren.ts"
+import type { MayHaveChildren } from "~/utils/ui/MayHaveChildren.ts"
 import type { MayHaveClass } from "~/utils/ui/MayHaveClass"
 
 export interface MayHaveChildrenAndClassName extends MayHaveChildren, MayHaveClass {}
