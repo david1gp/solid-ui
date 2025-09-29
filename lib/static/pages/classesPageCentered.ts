a30047a01efdb48694a32e9d38fdac9e418a2a5b
David Siewert <david1gruppenplan@gmail.com>
2025-09-29 13:49:42 +0600

    feat: add page centered classes

---
+++ lib/static/pages/classesPageCentered.ts
@@ -0,0 +1,12 @@
+import { classArr } from "~/utils/ui/classArr"
+
+export const classesPageHeightFull = "min-h-[80svh] sm:min-h-[90svh]"
+
+export const classesPageCentered = classArr(
+  "flex flex-col justify-center items-center my-auto", // align center
+)
+
+export const classesPageCenteredFull = classArr(
+  "flex flex-col justify-center items-center my-auto", // align center
+  classesPageHeightFull,
+)
