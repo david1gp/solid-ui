23eac514722ed56b5ed3d2039e2247fe48a8e3e4
David Siewert <david1gruppenplan@gmail.com>
2025-10-24 17:54:56 +0600

    styling(button): add disabled styles

--- lib/interactive/button/Button.tsx
+++ lib/interactive/button/Button.tsx
@@ -2,6 +2,7 @@ import type { Component, ComponentProps } from "solid-js"
 import { splitProps } from "solid-js"
 import { buttonCva2, type ButtonCvaProps } from "~ui/interactive/button/buttonCva"
 import { classesButtonClickAnimation } from "~ui/interactive/button/classesButtonClickAnimation"
+import { classesButtonDisabled } from "~ui/interactive/button/classesButtonDisabled"
 
 export interface ButtonProps extends ComponentProps<"button">, ButtonCvaProps {}
 
@@ -9,7 +10,7 @@ export const Button: Component<ButtonProps> = (p) => {
   const [, rest] = splitProps(p, ["variant", "size", "class", "type"])
   return (
     <button
-      class={buttonCva2(p.variant, p.size, classesButtonClickAnimation, p.class)}
+      class={buttonCva2(p.variant, p.size, classesButtonClickAnimation, p.disabled && classesButtonDisabled, p.class)}
       type={p.type ?? "button"}
       {...rest}
     />
