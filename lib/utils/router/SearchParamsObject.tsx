168171b4bf5ea8b8453298ea0a9ca8ca4ad13915
David Siewert <david1gruppenplan@gmail.com>
2025-10-25 09:22:26 +0600

    fix(router): remove added solid-router files, because it does not work on the consumer side

--- lib/utils/router/SearchParamsObject.tsx
+++
@@ -1,7 +0,0 @@
-import type { NavigateOptions } from "@solidjs/router"
-import type { SearchParams, SetSearchParams } from "node_modules/@solidjs/router/dist/types"
-
-export type SearchParamsObject = {
-  get: Partial<SearchParams>
-  set: (params: SetSearchParams, options?: Partial<NavigateOptions>) => void
-}
--- lib/utils/router/useSearchParamsObject.tsx
+++
@@ -1,7 +0,0 @@
-import { useSearchParams } from "@solidjs/router"
-import type { SearchParamsObject } from "~ui/utils/router/SearchParamsObject"
-
-export function useSearchParamsObject(): SearchParamsObject {
-  const [searchParams, setSearchParams] = useSearchParams()
-  return { get: searchParams, set: setSearchParams }
-}
