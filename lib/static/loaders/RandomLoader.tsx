428a98c2b6d8510e59fe69ed766e10afaaa1b55b
David Siewert <david1gruppenplan@gmail.com>
2025-10-29 08:23:29 +0600

    fix(loader): fix random loader should display multiple loaders

--- lib/static/loaders/RandomLoader.tsx
+++ lib/static/loaders/RandomLoader.tsx
@@ -1,15 +1,16 @@
 import { LoaderShuffle4Dots } from "~ui/static/loaders/LoaderShuffle4Dots"
+import { LoaderShuffle9Squares } from "~ui/static/loaders/LoaderShuffle9Squares"
+import { LoaderSpin4Square } from "~ui/static/loaders/LoaderSpin4Square"
 import type { HasClass } from "~ui/utils/HasClass"
 
 export function RandomLoader(p: HasClass) {
-  // const i = randomInteger(3)
-  // switch (i) {
-  //   default:
-  //     return <LoaderShuffle9Squares class={p.class} />
-  //   case 1:
-  //     return <LoaderShuffle4Dots class={p.class} />
-  //   case 2:
-  //     return <LoaderSpin4Square class={p.class} />
-  // }
-  return (<LoaderShuffle4Dots class={p.class} />)
+  const i = new Date().getSeconds() % 3
+  switch (i) {
+    default:
+      return <LoaderShuffle9Squares class={p.class} />
+    case 1:
+      return <LoaderShuffle4Dots class={p.class} />
+    case 2:
+      return <LoaderSpin4Square class={p.class} />
+  }
 }
