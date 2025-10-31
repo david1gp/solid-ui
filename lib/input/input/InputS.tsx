d9137d8f7332b33e3073ce826822ebdee7102827
David Siewert <david1gruppenplan@gmail.com>
2025-10-31 18:10:49 +0600

    fix(onInputS): call passed onInput method in addition to p.valueSignal.set

--- lib/input/input/InputS.tsx
+++ lib/input/input/InputS.tsx
@@ -1,4 +1,4 @@
-import { type ComponentProps, splitProps } from "solid-js"
+import { type ComponentProps, type JSX, splitProps } from "solid-js"
 import { Input } from "~ui/input/input/Input"
 import type { SignalObject } from "~ui/utils/createSignalObject"
 
@@ -7,6 +7,12 @@ export interface InputSProps extends ComponentProps<"input"> {
 }
 
 export function InputS(p: InputSProps) {
-  const [, rest] = splitProps(p, ["valueSignal"])
-  return <Input value={p.valueSignal.get()} onInput={(e) => p.valueSignal.set(e.currentTarget.value)} {...rest} />
+  const [, rest] = splitProps(p, ["valueSignal", "onInput"])
+  const onInputLocal: JSX.InputEventHandlerUnion<HTMLInputElement, InputEvent> = (e) => {
+    p.valueSignal.set(e.currentTarget.value)
+    if (p.onInput && typeof p.onInput === "function") {
+      p.onInput(e)
+    }
+  }
+  return <Input value={p.valueSignal.get()} onInput={onInputLocal} {...rest} />
 }
