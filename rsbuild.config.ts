82fa86279a8e7a77ba3d7546c36faeac2d721db1
David Siewert <david1gruppenplan@gmail.com>
2025-11-01 09:48:50 +0600

    fix(rsbuild): fix rsbuild build by adding pluginNodePolyfill, cause: nodejs imports in @adaptive-sm/utils

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@rsbuild/core"
 import { pluginBabel } from "@rsbuild/plugin-babel"
 import { pluginSolid } from "@rsbuild/plugin-solid"
+import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill"
 
 export default defineConfig({
   server: {
@@ -29,6 +30,10 @@ export default defineConfig({
     },
   },
   plugins: [
+    pluginNodePolyfill({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
     pluginBabel({
       include: /\.(?:jsx|tsx)$/,
     }),
