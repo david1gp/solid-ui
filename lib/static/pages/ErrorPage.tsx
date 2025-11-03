1fd9cf11e9a57dc8686daa0408d513a62ebbe365
David Siewert <david1gruppenplan@gmail.com>
2025-11-03 07:32:41 +0600

    refactor(error page): use interfaces, reduce classes, use simple "my" instead "min-h"

--- lib/static/pages/ErrorPage.tsx
+++ lib/static/pages/ErrorPage.tsx
@@ -1,26 +1,21 @@
 import { mdiAlertBoxOutline } from "@mdi/js"
 import { Icon1 } from "~ui/static/icon/Icon1"
+import type { HasTitle } from "~ui/utils/HasTitle"
 import type { MayHaveChildren } from "~ui/utils/MayHaveChildren"
 import type { MayHaveClass } from "~ui/utils/MayHaveClass"
+import type { MayHaveIcon } from "~ui/utils/MayHaveIcon"
+import type { MayHaveSubtitle } from "~ui/utils/MayHaveSubtitle"
 import { classMerge } from "~ui/utils/classMerge"
 
-export interface ErrorPageProps extends MayHaveClass, MayHaveChildren {
-  title: string
-  subtitle?: string
-  icon?: string
-  iconClass?: string
-  titleClass?: string
-  subtitleClass?: string
-}
+export interface ErrorPageProps extends HasTitle, MayHaveSubtitle, MayHaveIcon, MayHaveClass, MayHaveChildren {}
 
 export function ErrorPage(p: ErrorPageProps) {
   return (
     <div
       class={classMerge(
-        "flex flex-col items-center justify-center", // layout
-        "min-h-screen", // sizing
-        "bg-gray-50 dark:bg-gray-900", // bg
-        "p-4", // spacing
+        "flex flex-row items-center justify-center", // layout
+        "my-25",
+        // "min-h-[80dvh]", // sizing
         p.class,
       )}
     >
