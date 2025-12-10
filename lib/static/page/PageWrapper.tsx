ed7637b890e02657a5308ad9737a9ccdf7852b88
David Siewert <david1gruppenplan@gmail.com>
2025-12-10 12:23:04 +0600

    refactor(page wrapper): allow overwriting classes via props

--- lib/static/page/PageWrapper.tsx
+++ lib/static/page/PageWrapper.tsx
@@ -1,7 +1,7 @@
 import { classesBgGray } from "~ui/classes/classesBg"
 import type { MayHaveChildren } from "~ui/utils/MayHaveChildren"
 import type { MayHaveClass } from "~ui/utils/MayHaveClass"
-import { classArr } from "~ui/utils/classArr"
+import { classMerge } from "~ui/utils/classMerge"
 
 export interface PageWrapperProps extends MayHaveChildren, MayHaveClass {
   innerClass?: string
@@ -9,8 +9,8 @@ export interface PageWrapperProps extends MayHaveChildren, MayHaveClass {
 
 export function PageWrapper(p: PageWrapperProps) {
   return (
-    <div class={classArr("min-h-dvh w-full", classesBgGray, p.class)}>
-      <div class={classArr("max-w-7xl mx-auto", "dark:text-white", "p-4", p.innerClass)}>{p.children}</div>
+    <div class={classMerge("min-h-dvh w-full", classesBgGray, p.class)}>
+      <div class={classMerge("max-w-7xl mx-auto", "dark:text-white", "p-4", p.innerClass)}>{p.children}</div>
     </div>
   )
 }
