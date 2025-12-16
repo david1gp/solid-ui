9614e8951ae6455a26ed0dc838ae2604ddd7be0a
David Siewert <david1gruppenplan@gmail.com>
2025-12-16 19:09:30 +0600

    refactor(page): min-h-screen -> min-h-[90vh], keep 10% for navbar

--- lib/static/page/classesPageWrapper.ts
+++ lib/static/page/classesPageWrapper.ts
@@ -2,7 +2,7 @@ import { classArr } from "~ui/utils/classArr"
 
 export const classesPageWrapper = classArr(
   "flex flex-col items-center justify-center", // layout
-  "min-h-screen", // sizing
+  "min-h-[90vh]", // sizing
   "bg-gray-50 dark:bg-gray-900", // background
   "p-4", // spacing
 )
