56550e9cb392c919a81885897c1e84b3e9130066
David Siewert <david1gruppenplan@gmail.com>
2026-01-12 22:50:00 +0600

    fix(styles): fix some styles break because of semicolon

--- lib/static/styles/stylesBgDotted.ts
+++ lib/static/styles/stylesBgDotted.ts
@@ -1,6 +1,6 @@
 import type { JSX } from "solid-js"
 
 export const stylesBgDotted = {
-  "background-size": "10px 10px",
-  "background-image": "radial-gradient(circle at .75px .75px, #aeaeae69 .75px, #0000 0);",
+  "background-size": "15px 15px",
+  "background-image": "radial-gradient(circle at center, #aeaeae69 1px, #0000 0)",
 } as const satisfies JSX.CSSProperties
--- lib/static/styles/stylesBgSlashSparse.ts
+++ lib/static/styles/stylesBgSlashSparse.ts
@@ -1,5 +1,5 @@
 import type { JSX } from "solid-js"
 
 export const stylesBgSlashSparse = {
-  "background-image": "repeating-linear-gradient(135deg, #aeaeae69 0 1px, #0000 1px 24px);",
+  "background-image": "repeating-linear-gradient(135deg, #aeaeae69 0 1px, #0000 1px 24px)",
 } as const satisfies JSX.CSSProperties
