bee2b915c8d45578eaec231d94b71228c26a5255
David Siewert <david1gruppenplan@gmail.com>
2026-03-01 13:49:23 +0600

    feat(bg-styles): add dotted sparse

---
+++ lib/static/styles/stylesBgDottedSparse.ts
@@ -0,0 +1,7 @@
+import type { JSX } from "solid-js"
+
+export const stylesBgDottedSparse = {
+  "background-size": "10px 10px",
+  "background-repeat": "repeat",
+  "background-image": "radial-gradient(circle at .75px .75px, #78787821 .75px, #0000 0)",
+} as const satisfies JSX.CSSProperties
