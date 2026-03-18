e0b9784c87e992c5d884740c874ea60ac47a51cf
David Siewert <david1gruppenplan@gmail.com>
2026-03-18 12:55:08 +0600

    feat(badge): simplify, rename variants to be consistent with buttons, remove hover effects

--- lib/static/badge/badgeCva.tsx
+++ lib/static/badge/badgeCva.tsx
@@ -8,40 +8,32 @@ export const badgeVariant = {
   subtle: "subtle",
   outline: "outline",
   // colors 1
-  default: "default",
-  primary: "primary",
-  secondary: "secondary",
-  // secondary: "secondary",
-  destructive: "destructive",
+  contrast: "contrast",
   // colors 2
-  success: "success",
-  info: "info",
-  warning: "warning",
-  error: "error",
+  filledGreen: "filledGreen",
+  filledYellow: "filledYellow",
+  filledBlue: "filledBlue",
+  filledRed: "filledRed",
 } as const
 
 const baseClasses = classArr(
   "inline-flex items-center", // layout
   "text-sm", // text
   "px-2.5 py-0.5 border rounded-full", // padding
-  "transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2", // focus ring
+  "focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2", // focus ring
 )
 
 const variantClasses = {
-  default:
-    "bg-slate-900 hover:bg-slate-700 dark:hover:text-white border-transparent text-primary-foreground dark:bg-slate-50 dark:text-slate-900",
-  primary: "bg-primary hover:bg-primary/80 dark:hover:text-white border-transparent text-primary-foreground",
-  secondary: "bg-secondary hover:bg-secondary/80 border-transparent text-secondary-foreground",
-  success: "bg-green-500 text-white border-green-500",
-  error: "bg-red-600 text-white border-red-600 dark:border-red-600",
-  info: "bg-sky-500 text-white border-sky-500",
-  warning: "bg-yellow-500 text-white border-yellow-500",
-  destructive: "bg-destructive hover:bg-destructive/80 border-transparent text-destructive-foreground",
-  subtle: "bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100",
+  subtle: "bg-slate-100 text-slate-900 dark:bg-slate-700 dark:text-slate-100",
   outline: "text-foreground",
+  contrast: "bg-slate-900 border-transparent text-white dark:bg-slate-50 dark:text-slate-900",
+  filledGreen: "bg-green-500 text-white border-green-500",
+  filledYellow: "bg-yellow-500 text-white border-yellow-500",
+  filledBlue: "bg-sky-500 text-white border-sky-500",
+  filledRed: "bg-red-600 text-white border-red-600 dark:border-red-600",
 } as const satisfies Record<BadgeVariant, string>
 
-const defaultVariant = badgeVariant.default
+const defaultVariant = badgeVariant.outline
 
 export function badgeCva1(
   variant: BadgeVariant | null = defaultVariant,
