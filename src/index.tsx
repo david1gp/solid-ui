feed2a5761bab9bb448351492dd68b01a501ec77
David Siewert <david1gruppenplan@gmail.com>
2026-04-01 14:48:34 +0600

    fix(demos): hide learning solid in prod builds

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,20 @@
 import { demoList } from "#src/app/demos/demoList.js"
 import { NavDemo } from "#src/nav/NavDemo.jsx"
 import { generateDemoRoutes } from "#ui/demo_pages/generateDemoRoutes.jsx"
+import { isProdEnv } from "#ui/env/isProdEnv.js"
 import { Router } from "@solidjs/router"
 import { render } from "solid-js/web"
 import "./tailwind.css"
 
 const root = document.getElementById("root")
 
+const demoListProd = isProdEnv() ? (({ learning_solid: _, ...rest }) => rest)(demoList) : demoList
+
 const demoRoutesDef = [
   {
     // path: "/*",
     // component: LayoutWrapperDemo,
-    children: generateDemoRoutes(demoList, "", NavDemo),
+    children: generateDemoRoutes(demoListProd, "", NavDemo),
   },
 ]
 
