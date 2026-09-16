ec6e9b40c5e458da366afcf3d6aeae08a1f09b76
David Siewert <david1gruppenplan@gmail.com>
2026-09-16 02:59:13 +0200

    Revert "refactor(theme): remove toast side effects from theme rotation"
    This reverts commit 90f6a562e6b9a53ee8e828fc1188b5abdcbd6c44.

--- ui/interactive/theme/themeSignal.ts
+++ ui/interactive/theme/themeSignal.ts
@@ -1,17 +1,21 @@
+import { isServer } from "solid-js/web"
+import * as v from "valibot"
+import { themeButtonTextDefault } from "#ui/interactive/theme/ThemeButtonText.js"
 import {
   getThemeFromStorageOrBrowserPref,
   nextTheme2,
   setThemeToBrowser,
+  type ThemeVariant,
+  themeIcon,
   themeLocalStorageKey,
   themeSchema,
-  type ThemeVariant,
   themeVariant,
 } from "#ui/interactive/theme/themeVariant.js"
+import { toastAdd } from "#ui/interactive/toast/toastAdd.js"
+import { toastVariant } from "#ui/interactive/toast/toastVariant.js"
 import { createSignalObject } from "#ui/utils/createSignalObject.js"
-import { isServer } from "solid-js/web"
-import * as v from "valibot"
 
-export const themeSignal = createSignalObject<ThemeVariant>(themeVariant.light)
+export const themeSignal = createSignalObject<ThemeVariant>(themeVariant.os)
 
 export function themeInit() {
   if (isServer) return // Skip during SSR
@@ -47,5 +51,10 @@ function themeRegisterStorageListener() {
 export function themeRotate() {
   const currentTheme = themeSignal.get()
   const newTheme = nextTheme2(currentTheme)
+
+  const themeText = themeButtonTextDefault[newTheme]
+  const title = themeButtonTextDefault.setTheme(themeText)
+  const icon = themeIcon(newTheme)
+  toastAdd({ title, icon, variant: toastVariant.default })
   themeSet(newTheme, true)
 }
